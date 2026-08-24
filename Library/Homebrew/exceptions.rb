ad0d535335f9e8a691843d00b73f9013a4fe7e86
daycandido <220454006+daycandido@users.noreply.github.com>
2026-08-24 12:33:28 +0000

    Optimize intermediate array allocations by using `filter_map`

--- Library/Homebrew/exceptions.rb
+++ Library/Homebrew/exceptions.rb
@@ -745,7 +745,7 @@ class ErrorDuringExecution < RuntimeError
 
   sig { returns(String) }
   def stderr
-    Array(output).select { |type,| type == :stderr }.map(&:last).join
+    Array(output).filter_map { |type, line| line if type == :stderr }.join
   end
 end
 
@@ -820,7 +820,7 @@ class CyclicDependencyError < RuntimeError
   def initialize(strongly_connected_components)
     super <<~EOS
       The following packages contain cyclic dependencies:
-        #{strongly_connected_components.select { |packages| packages.count > 1 }.map(&:to_sentence).join("\n  ")}
+        #{strongly_connected_components.filter_map { |packages| packages.to_sentence if packages.count > 1 }.join("\n  ")}
     EOS
   end
 end
